1108cff40fc2bf96e84fb021afdd2735d35a8dd9
markihajnalka <markihajni@gmail.com>
2024-02-19 22:05:02 +0100

    IMP - datasource config class added

--- source/data_source/__init__.py
+++ source/data_source/__init__.py
@@ -1,5 +1,25 @@
+import json
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import Dict
+
+
+class DataSourceType(Enum):
+    SHEIN = "shein"
+
+
+class DataSourceConfig:
+    def __init__(self, ds_type: DataSourceType):
+        self._set_type(ds_type=ds_type)
+        self._set_config()
+
+    def _set_type(self, ds_type: DataSourceType):
+        self.type = ds_type
+
+    def _set_config(self):
+        self.config = json.load(
+            open(f"config/{self.type.value}.json"),
+        )
 
 
 class DataSource(ABC):
